c3fe44cd8804d4a93c8ca740c67003e711bc5c46
mitchmalvin1 <89574448+mitchmalvin1@users.noreply.github.com>
2022-04-02 22:44:17 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+//just another way of saying
+// var React = require("react");
+
+var ReactDOM = require("react-dom");
+
+ReactDOM.render(<h1>Hello World </h1>, document.getElementById("root"));
